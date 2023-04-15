a66bd3ee0c4e2e875dcc22d718d9af2c2a9af5b1
NikitaGasyuk <126286242+NikitaGasyuk@users.noreply.github.com>
2023-04-15 14:59:30 +0300

    Update image.cpp

--- image.cpp
+++ image.cpp
@@ -16,12 +16,12 @@ int main()
     }
 
     // Выводим параметры картинки в консоль
-    std::cout << "[i] channels: " << img_color.channels() << '\n';
-    std::cout << "[i] pixel depth: " << img_color.depth() << " bits\n";
-    std::cout << "[i] width: " << img_color.cols << " pixels\n";
-    std::cout << "[i] height: " << img_color.rows << " pixels\n";
-    std::cout << "[i] image size: " << img_color.total() * img_color.elemSize() << " bytes\n";
-    std::cout << "[i] width step: " << img_color.step << " bytes\n";
+    cout << "[i] channels: " << img_color.channels() << '\n';
+    cout << "[i] pixel depth: " << img_color.depth() << " bits\n";
+    cout << "[i] width: " << img_color.cols << " pixels\n";
+    cout << "[i] height: " << img_color.rows << " pixels\n";
+    cout << "[i] image size: " << img_color.total() * img_color.elemSize() << " bytes\n";
+    cout << "[i] width step: " << img_color.step << " bytes\n";
 
     // считываем и выводим картинку
     namedWindow("color image", WINDOW_AUTOSIZE);
